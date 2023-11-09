f463917ddf2809c54568ce2fd4d7c9d643423dd7
williamandrews2 <121400607+williamandrews2@users.noreply.github.com>
2023-11-09 15:28:04 -0600

    Completed exercise 3 reverseString.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let str = "";
+    let wordLength = word.length;
+    for(let i = wordLength-1; i>=0;i--){
+        str += word.charAt(i);
+    }
+    return str;
 };
+
+reverseString('hello');
 
 // Do not edit below this line
 module.exports = reverseString;
