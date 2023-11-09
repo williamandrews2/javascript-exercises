8a1e387d9c09e51322620e12944745462a03c65f
williamandrews2 <121400607+williamandrews2@users.noreply.github.com>
2023-11-09 15:19:36 -0600

    Completed exercise 2 repeatString.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    if(num>=0){
+        let str = '';
+        for(let i=0;i<num;i++) {
+            str += word;
+        }
+    return str;
+    }
+    
+    else {
+        return "ERROR";
+    }
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
