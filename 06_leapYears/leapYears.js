009749dacb3169344af4db53284044a9fc34cfe9
williamandrews2 <121400607+williamandrews2@users.noreply.github.com>
2023-11-09 18:20:40 -0600

    Completed exercise 6 leapYears.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    // Check to see if year if divisble by 4.
+    if(year%4==0 && year%100!=0) return true;
+    if(year%100==0 && year%400==0) return true;
+    else return false;
 };
 
 // Do not edit below this line
