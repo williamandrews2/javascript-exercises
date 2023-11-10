33e380b1323bfa4552c0b9d59ccdae2e87ac871b
williamandrews2 <121400607+williamandrews2@users.noreply.github.com>
2023-11-10 07:58:33 -0600

    Finished JS 6
    FInished javascript exercise 6

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+    tempInCelsius = Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10;
+    return tempInCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+    tempInFahrenheit = Math.round(((celsius * 9) / 5 + 32) * 10) / 10;
+    return tempInFahrenheit;
 };
 
 // Do not edit below this line
