8c2cdcbb48211cdb6ad6f9657ac370eda8313b49
williamandrews2 <121400607+williamandrews2@users.noreply.github.com>
2023-11-09 18:13:03 -0600

    Completed exercise 5 sumAll.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
 
+    if(start<0 || end<0 || !Number.isInteger(start) || !Number.isInteger(end)) {
+        return "ERROR";
+    }
+
+    else {
+        let result = 0;
+        let firstNumber = Math.min(start, end);
+        let lastNumber = Math.max(start, end);
+
+        // Add all the numbers between the two values by traversing through the loop.
+        for(firstNumber;firstNumber<=lastNumber;firstNumber++) {
+            result += firstNumber;
+        }
+
+        return result;
+    }
 };
 
 // Do not edit below this line
