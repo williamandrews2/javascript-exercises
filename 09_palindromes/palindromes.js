4edbcd6a25e54c9fe23df65e5916d71954a59618
williamandrews2 <121400607+williamandrews2@users.noreply.github.com>
2023-12-14 16:02:41 -0600

    Completed exercises 9, 10, 11, and 12.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let normalString = str.toLowerCase().replace(/[\W_]/g, "");
+  let reverseString = normalString.split("").reverse().join("");
+  return normalString === reverseString;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+// Solution is different but this still yields the same answer.
+const findTheOldest = function (people) {
+  const ordered = people.sort((a, b) =>
+    getAge(a.yearOfDeath, a.yearOfBirth) > getAge(b.yearOfDeath, b.yearOfBirth)
+      ? -1
+      : 1
+  );
+  return ordered[0];
+};
 
+const getAge = function (death, born) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - born;
 };
 
 // Do not edit below this line
